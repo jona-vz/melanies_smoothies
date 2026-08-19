d02f75e2895c1d84d3bb6ee99cec545455e9ab63
Jonathan Vasquez <91157504+jona-vz@users.noreply.github.com>
2026-08-18 22:26:47 -0600

    Update streamlit_app.py
    Exposing json from request

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ if ingredients_list:
     # New section to display smoothiefroot nutrition information
     import requests
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response)
+    st.text(smoothiefroot_response.json())
   
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
