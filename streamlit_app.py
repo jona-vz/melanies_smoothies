866a408378521d1312375b8bfbf010a76cf1a62b
Jonathan Vasquez <91157504+jona-vz@users.noreply.github.com>
2026-08-18 22:47:15 -0600

    Update streamlit_app.py
    Change the nutritional table place

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import os
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Custumize Your Smoothie!:cup_with_straw:")
@@ -37,6 +38,9 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+      
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
@@ -47,11 +51,6 @@ if ingredients_list:
     
     time_to_insert = st.button('Submit Order')
 
-    # New section to display smoothiefroot nutrition information
-    import requests
-    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    # st.text(smoothiefroot_response.json())
-    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
   
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
